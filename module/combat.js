31f8146560ea9bec6b4b0ba2768a40926c8999fd
Matteo Pieroni <matteopieroni6@gmail.com>
2021-01-08 19:10:10 +0000

    General files

--- module/combat.js
+++ module/combat.js
@@ -13,8 +13,8 @@ export const _getInitiativeFormula = function(combatant) {
   let nd = 1;
   let mods = "";
   
-  if (actor.getFlag("sw5efoundry", "halflingLucky")) mods += "r=1";
-  if (actor.getFlag("sw5efoundry", "initiativeAdv")) {
+  if (actor.getFlag("dnd5e", "halflingLucky")) mods += "r1=1";
+  if (actor.getFlag("dnd5e", "initiativeAdv")) {
     nd = 2;
     mods += "kh";
   }
@@ -22,46 +22,19 @@ export const _getInitiativeFormula = function(combatant) {
   const parts = [`${nd}d20${mods}`, init.mod, (init.prof !== 0) ? init.prof : null, (init.bonus !== 0) ? init.bonus : null];
 
   // Optionally apply Dexterity tiebreaker
-  const tiebreaker = game.settings.get("sw5efoundry", "initiativeDexTiebreaker");
+  const tiebreaker = game.settings.get("dnd5e", "initiativeDexTiebreaker");
   if ( tiebreaker ) parts.push(actor.data.data.abilities.dex.value / 100);
   return parts.filter(p => p !== null).join(" + ");
 };
 
-
-/* -------------------------------------------- */
-
-
 /**
- * TODO: A temporary shim until 0.7.x becomes stable
- * @override
+ * When the Combat encounter updates - re-render open Actor sheets for combatants in the encounter.
  */
-TokenConfig.getTrackedAttributes = function(data, _path=[]) {
-
-    // Track the path and record found attributes
-    const attributes = {
-      "bar": [],
-      "value": []
-    };
-
-    // Recursively explore the object
-    for ( let [k, v] of Object.entries(data) ) {
-      let p  = _path.concat([k]);
-
-      // Check objects for both a "value" and a "max"
-      if ( v instanceof Object ) {
-        const isBar = ("value" in v) && ("max" in v);
-        if ( isBar ) attributes.bar.push(p);
-        else {
-          const inner = this.getTrackedAttributes(data[k], p);
-          attributes.bar.push(...inner.bar);
-          attributes.value.push(...inner.value);
-        }
-      }
-
-      // Otherwise identify values which are numeric or null
-      else if ( Number.isNumeric(v) || (v === null) ) {
-        attributes.value.push(p);
-      }
-    }
-    return attributes;
-};
+Hooks.on("updateCombat", (combat, data, options, userId) => {
+  const updateTurn = ("turn" in data) || ("round" in data);
+  if ( !updateTurn ) return;
+  for ( let t of combat.turns ) {
+    const a = t.actor;
+    if ( t.actor ) t.actor.sheet.render(false);
+  }
+});
