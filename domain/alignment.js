458b5fcf6f2d2aaa2eaadc1d8ca7c54cb7c6f82b
Matteo Pieroni <matteopieroni6@gmail.com>
2021-07-18 18:41:20 +0100

    updated basic files

--- domain/alignment.js
+++ domain/alignment.js
@@ -13,14 +13,10 @@ export class Alignment {
 
   static computeTier(value) {
     if (value >= 0) {
-      // get the tier by diving the new value by 10
-      // and getting the lower value
       return Math.floor(value / this.max * 10);
     }
 
-    // get the tier by diving the new value by 10
-    // and getting the higher value
-    return Math.ceil(value / this.min * -10);
+    return Math.floor(value / this.min * -10);
   }
 
   static computeProgression(value, numberOfPoints) {
@@ -32,29 +28,19 @@ export class Alignment {
       throw new Error('Cannot modify without a number of points');
     }
 
-    // check current number of points sign
     const currentSign = value >= 0 ? 1 : -1;
-    // check modification sign
     const modificationSign = numberOfPoints > 0 ? 1 : -1;
-    // get current number of points
     const valueModulus = value * currentSign;
-    // get the current tier
     const tier = Math.floor(valueModulus / this.max * 10);
 
-    // if the tier is 0 simply return the new number of points
     if (tier === 0) {
       return value + numberOfPoints;
     }
 
-    // if the modification goes in the opposite direction of the current return the number of points
-    // when the direction is inverse we don't want to calculate with a different
-    // modifier
     if (currentSign * modificationSign < 0) {
       return value + numberOfPoints;
     }
 
-    // otherwise check if it goes towards the light side or the dark side
-    // and update using a linear increase dependent on the current tier
     const increase =  Math.max(1, numberOfPoints + Math.floor((tier + 1) / 3));
     const decrease =  Math.min(-1, numberOfPoints - Math.floor((tier + 1) / 3));
 
